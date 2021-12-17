d46f5dddcbcd32fba75a815ef1607f888125329c
Faiaz-Rahman <faiazrahman70@gmail.com>
2021-12-17 21:20:47 +0600

    Taxi-master

---
+++ config/mainconfig.js
@@ -0,0 +1,29 @@
+export const MainConfig = {
+    AppDetails: {
+        app_name: "Your App Name", 
+        app_description: "Your App Description Here",
+        app_identifier: "com.something.app",
+        ios_app_version: "1.0.0", 
+        android_app_version: 100
+    },
+    FirebaseConfig: {
+        apiKey: "XXXXXXXXXXXXXXXXX",
+        authDomain: "XXXXXXXXXXXXXXXXX",
+        databaseURL: "XXXXXXXXXXXXXXXXX",
+        projectId: "XXXXXXXXXXXXXXXXX",
+        storageBucket: "XXXXXXXXXXXXXXXXX",
+        messagingSenderId: "XXXXXXXXXXXXXXXXX",
+        appId: "XXXXXXXXXXXXXXXXX",
+        measurementId: "XXXXXXXXXXXXXXXXX"
+    },
+    Google_Map_API_Key: {
+        ios: "XXXXXXXXXXXXXXXXX",
+        android: "XXXXXXXXXXXXXXXXX",
+        web: "XXXXXXXXXXXXXXXXX"
+    },
+    facebookAppId: "XXXXXXXXXXXXXXXXX",
+    PurchaseDetails: {
+        CodeCanyon_Purchase_Code: "",
+        Buyer_Email_Address: ""
+    }
+}
